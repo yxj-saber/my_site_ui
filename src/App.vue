c42e0cd6e621d600b4806b81e01692ec50387747
yuxj3 <yuxj3@knownsec.com>
2020-09-16 18:53:39 +0800

    feat:create vue project

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<!--
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
+-->
+
+<template>
+  <div id="app">
+
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aisde</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+
+</script>
+
+<style>
+
+</style>
